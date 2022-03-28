342f3c4a37a5d1285970b0b028123c55581e6b50
zouqing <1916529007@qq.com>
2022-03-29 00:35:10 +0800

    引入css修饰界面

--- src/com/planett/learnt/java/login/LoginController.java
+++ src/com/planett/learnt/java/login/LoginController.java
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 public class LoginController {
     @FXML
     private PasswordField password_TextField;
-
+    // password_TextField得到了FXML文件中<TextField fx:id="password_TextField">的引用。
     @FXML
     private ImageView login_background;
 
@@ -30,7 +30,9 @@ public class LoginController {
 
     @FXML
     private Label message;
-
+    /*
+    *@FXML标签和FXML文件内容对应,即便不加此标签也能正确执行，但private必须用到此标签。
+     */
 
     @FXML
     void loginAction(ActionEvent event) throws SQLException, ClassNotFoundException {
@@ -50,4 +52,28 @@ public class LoginController {
 
     }
 
+
+    public PasswordField getPassword_TextField() {
+        return password_TextField;
+    }
+
+    public ImageView getLogin_background() {
+        return login_background;
+    }
+
+    public Image getLogin_background_image() {
+        return login_background_image;
+    }
+
+    public Button getLogin_Button() {
+        return login_Button;
+    }
+
+    public TextField getUserName_TextField() {
+        return userName_TextField;
+    }
+
+    public Label getMessage() {
+        return message;
+    }
 }
--- src/com/planett/learnt/java/main/MainApp.java
+++ src/com/planett/learnt/java/main/MainApp.java
@@ -1,19 +1,29 @@
 package com.planett.learnt.java.main;
 
+import com.planett.learnt.java.login.LoginController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.naming.ldap.Control;
+
 
 public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // 加载fxml文件
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getResource("/com/planett/learnt/resources/login_fx.fxml"));
         Parent root = fxmlLoader.load();
+
+        // 获取Controller实例
+        LoginController loginController = fxmlLoader.getController();
+
         Scene scene = new Scene(root);
+
+//        scene.getStylesheets().add(getClass().getResource("/com/planett/learnt/resources/login.css").toExternalForm());
         primaryStage.setTitle("Learn Together");
         primaryStage.setResizable(false);
         primaryStage.setScene(scene);
