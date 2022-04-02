30792020c1d997883627050053a8d81cbd83715e
zouqing <1916529007@qq.com>
2022-04-02 23:21:05 +0800

    2022/4/2 对主界面进行布局

--- src/com/planett/learnt/java/controller/MainController.java
+++ src/com/planett/learnt/java/controller/MainController.java
@@ -1,4 +1,29 @@
 package com.planett.learnt.java.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+
 public class MainController {
+
+    @FXML
+    private ToggleGroup leftbutton;
+
+    @FXML
+    private ToggleButton home_tbtn;
+
+    @FXML
+    private ToggleButton toChat_tbtn;
+
+    @FXML
+    void toHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toChatScence(ActionEvent event) {
+
+    }
+
 }
